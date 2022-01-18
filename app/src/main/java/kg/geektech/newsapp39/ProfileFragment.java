28e905c8538006f8e6961a363a8b423cc99091cb
Bema24 <bema.baktybekova03@gmail.com>
2022-01-18 18:26:05 +0300

    Initial commit

---
+++ app/src/main/java/kg/geektech/newsapp39/ProfileFragment.java
@@ -0,0 +1,49 @@
+package kg.geektech.newsapp39;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kg.geektech.newsapp39.databinding.FragmentProfileBinding;
+
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    binding.nullMan.setImageURI(uri);
+                }
+            });
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.nullMan.setOnClickListener(view1 -> {
+            mGetContent.launch("image/*");
+
+        });
+    }
+}
